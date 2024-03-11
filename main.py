c6d7111a97b07ce01e4b77631b47e694e8cac58e
Kaido Vetevoog <kaido.vetevoog@gmail.com>
2024-03-11 08:47:57 +0200

    Lisatud müügitulu tunnus näidetele

--- main.py
+++ main.py
@@ -16,20 +16,20 @@ def main():
 
     # result = Converter.convert_xls_to_classification_json(raw_data, classification_code)
 
-    # json_document = "JSON_files/samples/test_if_valid.json"
-    # json_schema = "JSON_files/schemas/test_if_schema.json"
+    json_document = "JSON_files/samples/EE0301010_sample.json"
+    json_schema = "JSON_files/schemas/EE0301010_schema.json"
 
-    # validation_result = Validator.validate_json(json_document, json_schema)
-    # print(validation_result)
+    validation_result = Validator.validate_json(json_document, json_schema)
+    print(validation_result)
 
-    xlm_file = FileOperations.open_file("XML_files/sample_reports/EE0301020_sample_report_stardard_small.xml")
-    is_xml_valid = Validator.validate_xml(xlm_file, "http://www.xbrl.org/taxonomy/int/gl/2015-03-25/plt/case-c-b-m/gl-plt-all-2015-03-25.xsd")
-    print ("validation result: ", is_xml_valid)
+    # xlm_file = FileOperations.open_file("XML_files/sample_reports/EE0301020_sample_report_stardard_small.xml")
+    # is_xml_valid = Validator.validate_xml(xlm_file, "http://www.xbrl.org/taxonomy/int/gl/2015-03-25/plt/case-c-b-m/gl-plt-all-2015-03-25.xsd")
+    # print ("validation result: ", is_xml_valid)
 
-    data_from_xml_file = Converter.convert_xml_to_dict('XML_files/sample_reports/EE0301020_sample_report_stardard_small.xml')
-    clean = XBRLGLValidator.format_xbrlgl_to_dict(data_from_xml_file)
-    is_equal = XBRLGLValidator.compare_debit_credit(clean)
-    print(is_equal)
+    # data_from_xml_file = Converter.convert_xml_to_dict('XML_files/sample_reports/EE0301020_sample_report_stardard_small.xml')
+    # clean = XBRLGLValidator.format_xbrlgl_to_dict(data_from_xml_file)
+    # is_equal = XBRLGLValidator.compare_debit_credit(clean)
+    # print(is_equal)
 
 
 
