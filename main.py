e4b0fb2a81c8fd80b273de367d53c8563b4e5899
Kaido Vetevoog <kaido.vetevoog@gmail.com>
2024-03-26 09:09:34 +0200

    header structure changes, additional mandatory element organizationDescription

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.converter import Converter
 from app.xbrlgl_validator import XBRLGLValidator
 from datetime import datetime
 
+
 def main():
     # json_document = "JSON_files/samples/classification_varagrupp.json"
     # json_schema = "JSON_files/schemas/classification_elements_schema.json"
@@ -14,26 +15,30 @@ def main():
     # #print(raw_data)
     # result = Converter.convert_xls_to_classification_json(raw_data, classification_code)
 
-    # json_document = "JSON_files/samples/EE0301020_sample_report_standard_small.json"
-    # json_schema = "JSON_files/schemas/EE0301020_schema.json"
+    """Validate JSON sample."""
+    json_document = "JSON_files/samples/EE0301020_sample_report_standard_small.json"
+    json_schema = "JSON_files/schemas/EE0301020_schema.json"
+    validation_result = Validator.validate_json(json_document, json_schema)
+    print(validation_result)
 
-    # json_document = "JSON_files/samples/EE0301020_sample_report_standard_small.json"
-    # json_schema = "JSON_files/schemas/EE0301020_schema.json"
-    # validation_result = Validator.validate_json(json_document, json_schema)
-
-    # xml_file = FileOperations.open_file("XML_files/sample_reports/EE0301020_sample_report_stardard_small.xml")
+    """Validate XML sample."""
+    # xml_file = FileOperations.open_file("XML_files/sample_reports/EE0302010_sample_report_stardard_small.xml")
     # is_xml_valid = Validator.validate_xml(xml_file, "http://www.xbrl.org/taxonomy/int/gl/2015-03-25/plt/case-c-b-m/gl-plt-all-2015-03-25.xsd")
     # print ("validation result: ", is_xml_valid)
+    # errors = Validator.get_xml_validation_errors(xml_file, "http://www.xbrl.org/taxonomy/int/gl/2015-03-25/plt/case-c-b-m/gl-plt-all-2015-03-25.xsd")
+    # print (errors)
 
     # data_from_xml_file = Converter.convert_xml_to_dict('XML_files/sample_reports/EE0301020_sample_report_stardard_small.xml')
     # clean = XBRLGLValidator.format_xbrlgl_to_dict(data_from_xml_file)
     # is_equal = XBRLGLValidator.compare_debit_credit(clean)
     # print(is_equal)
 
-    xls_file = "JSON_files/import/Subaccount_limitations.xlsx"
-    raw_data = FileOperations.load_subaccount_limitations_from_excel(xls_file, "Sheet1")
-    result = Converter.convert_xls_to_subaccount_limitations_json(raw_data)
-    FileOperations.save_dict_to_json_file(result, "subaccount_limitations")
+
+    """Generate bussiness rules json main->sub."""
+    # xls_file = "JSON_files/import/Subaccount_limitations.xlsx"
+    # raw_data = FileOperations.load_subaccount_limitations_from_excel(xls_file, "Sheet1")
+    # result = Converter.convert_xls_to_subaccount_limitations_json(raw_data)
+    # FileOperations.save_dict_to_json_file(result, "subaccount_limitations")
 
 
 if __name__ == "__main__":
